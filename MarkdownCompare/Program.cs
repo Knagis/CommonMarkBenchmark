0adc98f9b2c7c58f3c347ab0fd968e7b3b3e9178
Knagis <Knagis@users.noreply.github.com>
2014-09-18 20:50:29 +0300

    Added multiple source files. Also changed the way Stopwatch data is read because using Restart() gave completely wrong results.

--- MarkdownCompare/Program.cs
+++ MarkdownCompare/Program.cs
@@ -22,7 +22,6 @@ namespace MarkdownCompare
 
         static void Main(string[] args)
         {
-            var iterations = 30;
             var delegateNames = new string[]
             {
                 "Baseline",
@@ -39,41 +38,100 @@ namespace MarkdownCompare
                 (a, b) => b.Write(new MarkdownSharp.Markdown().Transform(a.ReadToEnd())),
                 (a, b) => b.Write(new MarkdownDeep.Markdown().Transform(a.ReadToEnd()))
             };
+
+            Console.WriteLine("All times are shown in milliseconds.");
+            Console.WriteLine();
+
+            var dir = new System.IO.DirectoryInfo(@"Tests");
+            if (!dir.Exists)
+                dir = new System.IO.DirectoryInfo(@"..\..\Tests");
+            if (!dir.Exists)
+            {
+                Console.WriteLine("Please create a folder named 'Tests' in the current directory and populate it with the source files.");
+                return;
+            }
+
+            foreach (var file in dir.GetFiles().OrderBy(o => o.Length))
+            {
+                ExecuteBenchmark(file, delegateNames, delegates);
+            }
+        }
+
+        private static void ExecuteBenchmark(System.IO.FileInfo file, string[] delegateNames, Action<System.IO.TextReader, System.IO.TextWriter>[] delegates)
+        {
+            // approximate the iterations based on file size (so that each file is read for ~10 MB)
+            var iterations = 10 * 1024 * 1024 / file.Length;
+            if (iterations < 3)
+                iterations = 3;
+            if (iterations > 10000)
+                iterations = 10000;
+
+            Console.Write(System.IO.Path.GetFileName(file.Name));
+            Console.Write("    ");
+            if (file.Length > 2000000)
+                Console.Write("{0:0.0} MB", file.Length / 1024M / 1024M);
+            else if (file.Length > 2000)
+                Console.Write("{0:0.0} KB", file.Length / 1024M);
+            else
+                Console.Write("{0} B", file.Length);
+            Console.WriteLine("   ({0} iterations)", iterations);
+            Console.WriteLine();
+            Console.WriteLine("{0,20} {1,8} {2,6}   {3}", "Library", "Total", "Each", "vs Baseline");
+            Console.WriteLine("--------------------------------------------------");
+
             var results = new long[delegates.Length];
+            var errors = new string[delegates.Length];
             var sw = new System.Diagnostics.Stopwatch();
+            sw.Start();
             for (var i = -1; i < iterations; i++)
             {
                 for (var j = 0; j < delegates.Length; j++)
                 {
                     // if the particular parser takes too long, just assume it will take just as long next time and skip it.
-                    if (results[j] > 10000)
+                    if (results[j] > 20000)
                     {
                         results[j] = (long)((decimal)results[j] / i * (i + 1));
                         continue;
                     }
 
-                    using (var reader = new System.IO.StreamReader(@"..\..\spec.txt"))
+                    using (var reader = new System.IO.StreamReader(file.FullName))
                     using (var writer = new System.IO.StringWriter())
                     {
-                        sw.Restart();
+                        var start = sw.ElapsedMilliseconds;
 
-                        delegates[j](reader, writer);
+                        try
+                        {
+                            delegates[j](reader, writer);
+                        }
+                        catch(Exception ex)
+                        {
+                            errors[j] = ex.Message;
+                        }
 
-                        sw.Stop();
+                        var finish = sw.ElapsedMilliseconds;
+
+                        // skip the first iteration - assume it is a warmup
+                        if (i >= 0)
+                            results[j] += (finish - start);
                     }
-
-                    // skip the first iteration - assume it is a warmup
-                    if (i >= 0)
-                        results[j] += sw.ElapsedMilliseconds;
                 }
             }
 
             for (var j = 0; j < delegates.Length; j++)
             {
                 Console.Write("{0, 20: 0}", delegateNames[j]);
+                Console.Write(" {0, 8: 0}", results[j]);
                 Console.Write(" {0, 6: 0}", (decimal)results[j] / iterations);
-                Console.WriteLine("  {0:P0}", (decimal)results[j] / results[0]);
+                Console.WriteLine("   {0:P0}         ", (decimal)results[j] / results[0]);
+                if (errors[j] != null)
+                {
+                    Console.WriteLine(delegateNames[j] + " failed: " + errors[j]);
+                    Console.WriteLine();
+                }
             }
+
+            Console.WriteLine("--------------------------------------------------");
+            Console.WriteLine();
         }
     }
 }
